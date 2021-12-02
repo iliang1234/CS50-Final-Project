80c212fb4ca5a7698ef490c7a582019f76c86522
iliang1234 <89923570+iliang1234@users.noreply.github.com>
2021-12-02 14:05:56 -0500

    app.py update
    app.py can now print the track name and artist name given some sentence input

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="YOUR_APP_CLIENT_ID",
-                                                           client_secret="YOUR_APP_CLIENT_SECRET"))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="5143c26fbbcd46918a5fa549ce3199d4",
+                                                           client_secret="2f1ac0c2c08a4226a9a88b111b2fa3cf"))
 
-results = sp.search(q='weezer', limit=20)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
+sample_sentence = input("Enter a sentence: ")
+sentence_list = sample_sentence.split(" ")
+print(sentence_list)
+
+# Print track names that contains each word from the sentence input
+for word in sentence_list:
+    results = sp.search(q=word, type="track", limit=1)
+    for track in enumerate(results['tracks']['items']):
+        # Print track name and track artist(s) -- holy shit this part took too fucking long
+        artists = (track[1]['artists'])[0]['name']
+        track_name = track[1]['name']
+        print(track_name + " by " + artists)
+
+"""
+    TODO FOR HTML/PYTHON LINK:
+        Highlight the word in the track titles (they might not be words on their own; i.e. 'is' being apart of 'island')
+        Hyperlink each song (I can get the link from python but Silas needs to implement it to the HTML)
+"""
